baf56383e0e8cf8f9feeaebcd07afb4d1482f743
Elliotconstantine <pangkaiyuan@stu2025.jnu.edu.cn>
2026-04-21 10:34:43 +0800

    完成了猜数字游戏，很好玩

---
+++ guess_number.py
@@ -0,0 +1,12 @@
+import random
+secret_number = random.randint(1, 100)
+guess = 0
+print("欢迎来到猜数字游戏！我想了一个1到100之间的数字。")
+while guess != secret_number:
+    guess = int(input("请输入你的猜测："))
+    if guess < secret_number:
+        print("太小了！再试一次。")
+    elif guess > secret_number:
+        print("太大了！再试一次。")
+    else:
+        print("恭喜你！猜对了！")
